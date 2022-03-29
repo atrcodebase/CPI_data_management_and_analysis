fe419cad169367b7959655cdeb7c6c0b186841da
Shahim Nadery <Shahim_Nadery@yahoo.com>
2022-03-29 13:03:15 +0430

    Added the script for Rejection log and fixed the issue with IME V2 logs!

--- 4- apply cleaning log.R
+++ 4- apply cleaning log.R
@@ -1,6 +1,7 @@
 # load required packages --------------------------------------------------
 source("R/required_packages.R")
 source("R/custom_functions.R")
+`%notin%` <- Negate(`%in%`)
 
 # read merged data sets --------------------------------------------------
 path <- "output/merged data forms/"
@@ -16,11 +17,14 @@ for (form in forms) {
 names(data)
 rm("file_name", "form", "forms", "path", "check_column_names", "dates_to_character", "read_excel_func")
 
+data[["CPI_Border_Count_of_Transport_Traffic_Dataset"]][["data"]] <- data[["CPI_Border_Count_of_Transport_Traffic_Dataset"]][["data"]] %>%
+  mutate(Number_of_TPMA_visits_by_Sector = as.numeric(Number_of_TPMA_visits_by_Sector))
 # read the cleaning log --------------------------------------------------
 url <- "https://docs.google.com/spreadsheets/d/1GxSV76XoQSBF45zmd8r1LX7r3cUQWB1I7aJ7b9KA51U/edit#gid=0"
 # browseURL(url)
 googlesheets4::gs4_deauth()
 cleaning_log <- googlesheets4::read_sheet(url, sheet = "cleaning_log", col_types = "c")
+rejection_log <- googlesheets4::read_sheet(url, sheet = "rejection_log", col_types = "c")
 count(cleaning_log, changed)
 cleaning_log <- cleaning_log %>% filter(changed == "Yes") %>% 
   mutate(uuid = str_trim(uuid))
@@ -39,9 +43,13 @@ for (i in names(data)) {
   }
 }
 
+#Cleaning Log
 unique(cleaning_log$dataset_name)[!unique(cleaning_log$dataset_name) %in% names(data)]
 unique(cleaning_log$sheet_name)[!unique(cleaning_log$sheet_name) %in% sheet_names]
 unique(cleaning_log$question)[!unique(cleaning_log$question) %in% col_names]
+#Rejection Log
+unique(rejection_log$dataset_name)[!unique(rejection_log$dataset_name) %in% names(data)]
+unique(rejection_log$sheet_name)[!unique(rejection_log$sheet_name) %in% sheet_names]
 
 rm("col", "col_names", "i", "sheet", "sheet_names", "url")
 
@@ -49,34 +57,41 @@ rm("col", "col_names", "i", "sheet", "sheet_names", "url")
 n <- 1
 col_names <- c()
 col_types <- c()
-for (i in names(data)) {
-  for (sheet in names(data[[i]])) {
-    print(paste0(n, "-Get column names/types from ", i, ":", sheet))
+form_name <- c()
+for (form_i in names(data)) {
+  for (sheet in names(data[[form_i]])) {
+    print(paste0(n, "-Get column names/types from ", form_i, ":", sheet))
     n <- n+1
-    for (col in colnames(data[[i]][[sheet]])) {
-      if (!col %in% col_names) {
-        column_type <- typeof(data[[i]][[sheet]][[col]])
-        col_types <- c(col_types, column_type)
-        col_names <- c(col_names, col) 
-      }
+    for (col in colnames(data[[form_i]][[sheet]])) {
+      column_type <- typeof(data[[form_i]][[sheet]][[col]])
+      col_types <- c(col_types, column_type)
+      col_names <- c(col_names, col) 
+      form_name <- c(form_name, form_i)
     }
   }
 }
 
 ques_types <- data.frame(
   question = col_names,
-  question_type = col_types
-)
+  question_type = col_types,
+  dataset_name = form_name
+) %>% unique()
 
 cleaning_log <- cleaning_log %>% 
   select(-question_type) %>% 
-  left_join(ques_types, by = "question") %>% 
+  left_join(ques_types, by = c("question", "dataset_name")) %>% 
   relocate(question_type, .after = question)
 
-rm("col", "col_names", "col_types", "column_type", "i", "n", "sheet")
+rm("col_names", "col_types", "column_type", "n", "form_name")
 
-data[["CPI_Border_Count_of_Transport_Traffic_Dataset"]][["data"]] <- data[["CPI_Border_Count_of_Transport_Traffic_Dataset"]][["data"]] %>%
-  mutate(Number_of_TPMA_visits_by_Sector = as.numeric(Number_of_TPMA_visits_by_Sector))
+# apply rejection log -------------------------------------------------
+for (rowi in 1:nrow(rejection_log)){
+  form_i <- rejection_log$dataset_name[rowi]
+  sheet_i <- rejection_log$sheet_name[rowi]
+  uuid_i <- rejection_log$uuid[rowi]
+  
+  data[[form_i]][[sheet_i]] <- data[[form_i]][[sheet_i]] %>% filter(KEY %notin% uuid_i)
+}
 
 # apply cleaning log -------------------------------------------------
 count <- 1
@@ -103,7 +118,6 @@ for (rowi in 1:nrow(cleaning_log)){
 
 names(data)
 # export files  -------------------------------------------------
-
 
 ## data sets
 for (i in names(data)) {
